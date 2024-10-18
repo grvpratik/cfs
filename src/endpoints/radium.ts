0d02eca03e7fb0cb265a2c7152aee1e6900472bf
grvPratik <pratikgauravv@gmail.com>
2024-10-18 12:26:51 +0530

    send message raydium

--- src/endpoints/radium.ts
+++ src/endpoints/radium.ts
@@ -1,5 +1,11 @@
-export default async function raydium(c: any) {
+import { sendMessage } from "utils/telegram"
 
+export default async function raydium(c: any) {
+    const { BOT_TOKEN } = c.env
+    const raw = c.req.json()
+    const data = raw[0]
+    if (data) await sendMessage(5916990259, data, BOT_TOKEN)
+console.log("RAYDIUM")
     console.log(await c.req.json())
     return c.text('OK')
 }
